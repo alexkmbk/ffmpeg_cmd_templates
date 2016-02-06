f5289292e7bb561082aa4a24d9604642988d22ec
alexkmbk <alexkmbkdr1@gmail.com>
2016-02-06 20:23:09 +0600

    first commit

---
+++ scripts.bat
@@ -0,0 +1,13 @@
+//  Конвертация DVD файлов в один видео файл в h.264 формате видео и mp3 формате звука, контейнер mp4 
+ffmpeg -i concat:"D:\VIDEO_TS\VTS_01_0.VOB|D:\VIDEO_TS\VTS_01_1.VOB|D:\VIDEO_TS\VTS_01_2.VOB|D:\VIDEO_TS\VTS_01_3.VOB|D:\VIDEO_TS\VTS_01_4.VOB|D:\VIDEO_TS\VTS_01_5.VOB" -loglevel panic -vcodec libx264 -threads 0 -b 2000k -acodec libmp3lame -ab 320k "D:\VIDEO_TS\compressed\out.mp4"
+
+// Конвертация любых видео-файлов с расширением .MOV  в файл в h.264 формате видео и mp3 формате звука, контейнер mp4 
+// обрабатываются все файлы в текущей директории а также во всех вложенных
+// если для файла с расширением .mov уже существует соответствующий файл с расширением mp4, то он пропускается и не обрабатывается 
+// for /R %%G in (*.MOV) do IF NOT EXIST "D:\bin\ffmpeg\bin\%%~nG.mp4" ffmpeg -i "%%G" -loglevel panic -vcodec libx264 -threads 0 -b 2000k -acodec libmp3lame -ab 320k "D:\bin\ffmpeg\bin\%%~nG.mp4"
+
+
+// Извлечение из любых файлов с расширением *.m4a аудиодорожек и сохранение в mp3 формате с битрейтом 256k. 
+// обрабатываются все файлы в текущей директории, а также во всех вложенных
+// если для файла с расширением .mov уже существует соответствующий файл с расширением mp4, то он пропускается и не обрабатывается 
+for /R %%G in (*.m4a) do IF NOT EXIST "D:\bin\ffmpeg\bin\%%~nG.mp3" ffmpeg -i "%%G" -loglevel panic -acodec libmp3lame -ab 256k "D:\bin\ffmpeg\bin\%%~nG.mp3"
